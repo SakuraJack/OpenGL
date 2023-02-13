a06ee5c17f5b101bcd71fd51f609b28d6b57a0d5
SakuraJack <972688167@qq.com>
2023-02-13 21:57:20 +0800

    draw triangle

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -17,7 +17,17 @@ int main()
 	glfwMakeContextCurrent(window);
 
 	while (!glfwWindowShouldClose(window)) {
+		glClear(GL_COLOR_BUFFER_BIT);
 
+		glBegin(GL_TRIANGLES);
+		glVertex2f(-0.5f, -0.5f);
+		glVertex2f(0.0f, 0.5f);
+		glVertex2f(0.5f, -0.5f);
+		glEnd();
+
+		glfwSwapBuffers(window);
+
+		glfwPollEvents();
 	}
 
 	glfwTerminate();
